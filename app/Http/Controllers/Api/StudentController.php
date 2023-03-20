abe67332592429de298fbad68a0a077886a52230
mustafa <mustafa.bhm3@gmail.com>
2023-03-19 22:04:00 -0600

    route for adding students to db with validation of inputs

--- app/Http/Controllers/Api/StudentController.php
+++ app/Http/Controllers/Api/StudentController.php
@@ -3,11 +3,65 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Student;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class StudentController extends Controller
 {
     public function index(){
-        return 'this is a test';
+
+        $students = Student::all();
+        if($students->count()>0){
+            $data = [
+                'status' => 200,
+                'students'=> $students,
+            ];
+
+            return response()->json($data, 200);
+        }else{
+            $data = [
+                'status' => 404,
+                'message'=> 'No Data found !',
+            ];
+
+            return response()->json($data, 200);
+        }
+
+    }
+
+    public function store(Request $request){
+
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:191',
+            'course' => 'required|string|max:191',
+            'email' => 'required|email|max:191',
+            'phone' => 'required|digits:10',
+        ]);
+
+        if($validator->fails()){
+            return response()->json([
+                'status'=> 422,  // 422 => input error
+                'errors'=> $validator->messages()
+            ], 422);
+        } else {
+            $student = Student::create([
+                'name'=> $request->name,
+                'course'=> $request->course,
+                'email'=> $request->email,
+                'phone'=> $request->phone,
+            ]);
+            if($student){
+                return response()->json([
+                    'status' =>200,
+                    'message'=> 'Student Created Successfully !'
+                ], 200);
+            }else {
+                return response()->json([
+                    'status' =>500,
+                    'message'=> 'somthing went wrong'
+                ], 500);
+            }
+        }
     }
 }
